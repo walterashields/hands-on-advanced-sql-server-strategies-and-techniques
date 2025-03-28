206afec48e7aaa4c667e3c00c7cef61cf1aee098
Walter Shields <walterashields@gmail.com>
2025-03-27 22:17:32 -0400

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Wait for SQL Server to be ready
-echo "Waiting for SQL Server to start..."
+# Wait for SQL Server
 for i in {1..50};
 do
   if /opt/mssql-tools/bin/sqlcmd -S db -U sa -P "YourStrong!Passw0rd" -Q "SELECT 1" &> /dev/null
   then
-    echo "SQL Server is ready"
     break
-  else
-    echo "Not ready yet..."
-    sleep 1
   fi
+  sleep 1
 done
 
-# Run the setup script
-echo "Running setup script..."
-/opt/mssql-tools/bin/sqlcmd -S db -U sa -P "YourStrong!Passw0rd" -d master -i setup-moviesdb.sql
+# Execute setup script from mounted .devcontainer folder
+/opt/mssql-tools/bin/sqlcmd -S db -U sa -P "YourStrong!Passw0rd" -d master -i /container-config/setup-moviesdb.sql
