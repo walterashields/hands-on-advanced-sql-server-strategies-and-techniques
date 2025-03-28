6c23f8fe5b298934d7d619cbf369ce2d0a312050
Walter Shields <walterashields@gmail.com>
2025-03-28 13:33:11 -0400

    Update entrypoint.sh

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Start SQL Server in the background
-/opt/mssql/bin/sqlservr &
-
-# Wait for SQL Server to start
-echo "Waiting for SQL Server to start..."
-sleep 15
-
-# Run the setup script to create MoviesDB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'YourStrong!Passw0rd' -i /container-config/setup-moviesdb.sql
-
-# Keep the container running
-wait
+/opt/mssql/bin/sqlservr
