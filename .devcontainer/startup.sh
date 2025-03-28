bb969b3405a8db266f86b9fc29fcd1991871509b
Walter Shields <walterashields@gmail.com>
2025-03-27 20:20:00 -0400

    Update startup.sh 03

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -3,5 +3,5 @@
 echo "Waiting for SQL Server to start..."
 sleep 15
 
-echo "Running MoviesDB.sql script..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'YourStrong@Passw0rd' -d master -i /workspace/init/MoviesDB.sql
+echo "Running setup-moviesdb.sql script..."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'YourStrong@Passw0rd' -d master -i /workspace/.devcontainer/setup-moviesdb.sql
